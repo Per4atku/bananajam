9dacbdb87c418d8f6602feab6bd9af2c4a42416d
Per4atku <artcchi@gmail.com>
2024-06-27 17:54:43 +1000

    [+] MaxWidthWrapper

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const maxWidthProperties = (className?: string) => {
+  return cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className);
+};
+
+export default MaxWidthWrapper;
