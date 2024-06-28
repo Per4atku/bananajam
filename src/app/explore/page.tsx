429f91d69db5fa115051fa5f977fd6ba988c2902
Per4atku <artcchi@gmail.com>
2024-06-29 03:11:08 +1000

    🍌 [+] Album.tsx, Track.tsx

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,4 +1,6 @@
+import Album from "@/components/items/Album";
 import Artist from "@/components/items/Artist";
+import Track from "@/components/items/Track";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Search from "@/components/search/Search";
 import { Separator } from "@/components/ui/separator";
@@ -22,27 +24,24 @@ const Explore = ({
             imageURL="https://i.scdn.co/image/f0a39a8a196a87a7236bdcf8a8708f6d5d3547cc"
           />
           <Separator />
-          <Artist
+          <Album
             isAdded={true}
-            name="Pink Floyd"
-            genres={["Psychedelic Rock", "Prog rock"]}
-            imageURL="https://i.scdn.co/image/f0a39a8a196a87a7236bdcf8a8708f6d5d3547cc"
+            name={"Wish You Were Here"}
+            artistName={"Pink Floyd"}
+            imageURL={
+              "https://i.scdn.co/image/ab67616d0000b2731a84d71391df7469c5ab8539"
+            }
           />
           <Separator />
-          <Artist
+          <Track
             isAdded={true}
-            name="Pink Floyd"
-            genres={["Psychedelic Rock", "Prog rock"]}
-            imageURL="https://i.scdn.co/image/f0a39a8a196a87a7236bdcf8a8708f6d5d3547cc"
+            name={"Wish You Were Here"}
+            artistName={"Pink Floyd"}
+            imageURL={
+              "https://i.scdn.co/image/ab67616d0000b2731a84d71391df7469c5ab8539"
+            }
+            albumName="Wish You Were Here"
           />
-          <Separator />
-          <Artist
-            isAdded={true}
-            name="Pink Floyd"
-            genres={["Psychedelic Rock", "Prog rock"]}
-            imageURL="https://i.scdn.co/image/f0a39a8a196a87a7236bdcf8a8708f6d5d3547cc"
-          />
-          <Separator />
         </>
       ) : (
         <>
---
+++ src/components/items/Track.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { toast } from "sonner";
+import { useState } from "react";
+import { BookmarkPlus, Check } from "lucide-react";
+
+interface TrackProps {
+  imageURL: string;
+  name: string;
+  isAdded: boolean;
+  artistName: string;
+  albumName: string;
+}
+
+const Track = ({
+  imageURL,
+  name,
+  isAdded,
+  artistName,
+  albumName,
+}: TrackProps) => {
+  const [isOptimisticallyAdded, setIsOptimisticallyAdded] =
+    useState<boolean>(isAdded);
+  return (
+    <>
+      <div className="flex w-full rounded-md p-2 duration-200 items-center my-3 hover:bg-accent">
+        <button
+          onClick={() => toast(name)}
+          className="flex items-center w-full py-2  overflow-hidden"
+        >
+          <Avatar className="h-12 w-12 rounded-sm">
+            <AvatarImage src={imageURL} alt={name} />
+            <AvatarFallback className="rounded-sm">BJ</AvatarFallback>
+          </Avatar>
+          <div className="ml-3 overflow-hidden">
+            <h3
+              className="text-xl font-medium whitespace-nowrap 
+                        overflow-hidden overflow-ellipsis text-left sm:text-2xl
+                        
+          "
+            >
+              {name}
+            </h3>
+            <p className="text-left text-muted-foreground overflow-hidden overflow-ellipsis whitespace-nowrap w-full flex justify-between  text-xs sm:text-sm ">
+              {artistName} • {albumName}
+            </p>
+          </div>
+        </button>
+        <form
+          action={(formData: FormData) => {
+            setIsOptimisticallyAdded((prev) => !prev);
+          }}
+        >
+          <Button
+            variant={isOptimisticallyAdded ? "secondary" : "default"}
+            onClick={() => toast("follow!")}
+          >
+            {isOptimisticallyAdded ? (
+              <>
+                <Check size={18} /> <p className="ml-1.5">In Library</p>
+              </>
+            ) : (
+              <>
+                <BookmarkPlus size={18} /> <p className="ml-1.5">Add</p>
+              </>
+            )}
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Track;
