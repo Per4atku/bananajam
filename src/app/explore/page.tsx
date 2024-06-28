5b471e6ba58aa393b4ac0763ae45212af947a959
Per4atku <artcchi@gmail.com>
2024-06-28 14:09:32 +1000

    🍌 minor

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -12,7 +12,7 @@ const Explore = ({
   return (
     <MaxWidthWrapper>
       <Search placeholder="Search for..." />
-      {searchParams?.query && (
+      {searchParams?.query ? (
         <>
           <Artist
             isAdded={true}
@@ -34,6 +34,11 @@ const Explore = ({
             name="Pink Floyd"
             imageURL="https://i.scdn.co/image/f0a39a8a196a87a7236bdcf8a8708f6d5d3547cc"
           />
+        </>
+      ) : (
+        <>
+          <h1 className="font-bold text-3xl mt-12">You might also like...</h1>
+          <p className="mt-12">Some bullshit albums</p>
         </>
       )}
     </MaxWidthWrapper>
