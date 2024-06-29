5c9b193e376bb998caed54337092e74c8326977a
Per4atku <artcchi@gmail.com>
2024-06-29 23:49:41 +1000

    🍌 Minor

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -5,7 +5,6 @@ interface SearchContentProps {
   query: string;
 }
 
-//add "async"
 const SearchResults = async ({ query }: SearchContentProps) => {
   const artists = await musicDataAPI.getArtists(5, query, 0);
   console.log(artists);
@@ -19,7 +18,7 @@ const SearchResults = async ({ query }: SearchContentProps) => {
               name={artist.name}
               genres={artist.genres}
               isAdded={false}
-              imageURL={artist.images[2].url}
+              imageURL={artist.images[0].url}
             />
           </>
         ))}
