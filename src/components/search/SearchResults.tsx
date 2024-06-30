3169f216ee3e9e34e294c03852dd5505480f5e06
Per4atku <artcchi@gmail.com>
2024-07-01 02:48:01 +1000

    🍌 SearchResults.tsx - component recomposition

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -11,72 +11,80 @@ interface SearchContentProps {
 
 const ITEMS_PER_SCROLL = 9;
 
-const SearchResults = async ({ query, sort_by }: SearchContentProps) => {
+const ArtistResults = async ({ query }: { query: string }) => {
+  const artists = await musicDataAPI.getArtists(ITEMS_PER_SCROLL, query, 0);
+  return (
+    <>
+      {artists &&
+        artists.map((artist, index) => (
+          <>
+            <Artist
+              key={index}
+              name={artist.name}
+              genres={artist.genres}
+              isAdded={false}
+              imageURL={artist?.images[2]?.url}
+            />
+            <Separator />
+          </>
+        ))}
+    </>
+  );
+};
+
+const AlbumResults = async ({ query }: { query: string }) => {
+  const albums = await musicDataAPI.getAlbums(ITEMS_PER_SCROLL, query, 0);
+
+  return (
+    <>
+      {albums &&
+        albums.map((album, index) => (
+          <>
+            <Album
+              key={index}
+              name={album.name}
+              artistName={album.artists.map((artist) => artist.name).toString()}
+              isAdded={false}
+              imageURL={album?.images[2]?.url}
+            />
+            <Separator />
+          </>
+        ))}
+    </>
+  );
+};
+
+const TrackResults = async ({ query }: { query: string }) => {
+  const tracks = await musicDataAPI.getTracks(ITEMS_PER_SCROLL, query, 0);
+
+  return (
+    <>
+      {tracks &&
+        tracks.map((track, index) => (
+          <>
+            <Track
+              key={index}
+              name={track.name}
+              imageURL={track.album.images[2].url}
+              isAdded={false}
+              artistName={track.artists.map((artist) => artist.name).toString()}
+              albumName={track.album.name}
+            />
+            <Separator />
+          </>
+        ))}
+    </>
+  );
+};
+
+const SearchResults = ({ query, sort_by }: SearchContentProps) => {
   if (query)
     if (sort_by === "artist") {
-      const artists = await musicDataAPI.getArtists(ITEMS_PER_SCROLL, query, 0);
-      return (
-        <>
-          {artists &&
-            artists.map((artist, index) => (
-              <>
-                <Artist
-                  key={index}
-                  name={artist.name}
-                  genres={artist.genres}
-                  isAdded={false}
-                  imageURL={artist?.images[2]?.url}
-                />
-                <Separator />
-              </>
-            ))}
-        </>
-      );
+      return <ArtistResults query={query} />;
     } else if (sort_by === "album") {
-      const albums = await musicDataAPI.getAlbums(ITEMS_PER_SCROLL, query, 0);
-
-      return (
-        <>
-          {albums &&
-            albums.map((album, index) => (
-              <>
-                <Album
-                  key={index}
-                  name={album.name}
-                  artistName={album.artists
-                    .map((artist) => artist.name)
-                    .toString()}
-                  isAdded={false}
-                  imageURL={album?.images[2]?.url}
-                />
-                <Separator />
-              </>
-            ))}
-        </>
-      );
+      return <AlbumResults query={query} />;
     } else if (sort_by === "track") {
-      const tracks = await musicDataAPI.getTracks(ITEMS_PER_SCROLL, query, 0);
-
-      return (
-        <>
-          {tracks &&
-            tracks.map((track, index) => (
-              <>
-                <Track
-                  key={index}
-                  name={track.name}
-                  imageURL={track.album.images[2].url}
-                  isAdded={false}
-                  artistName={track.artists
-                    .map((artist) => artist.name)
-                    .toString()}
-                  albumName={track.album.name}
-                />
-                <Separator />
-              </>
-            ))}
-        </>
-      );
+      return <TrackResults query={query} />;
     } else return <></>;
 };
 
