dee6ee3f33a45621988e3464052043b863d46c98
Per4atku <artcchi@gmail.com>
2024-07-14 02:41:51 +1000

    🍌 [+] add useClickOutside for Drawer

--- src/app/explore/@modal/(..)artist/[artistId]/page.tsx
+++ src/app/explore/@modal/(..)artist/[artistId]/page.tsx
@@ -3,7 +3,7 @@
 import { Dialog } from "@/components/ui/dialog";
 import { Drawer, DrawerContent } from "@/components/ui/drawer";
 import { DialogContent } from "@radix-ui/react-dialog";
-import { useKeyPress } from "@siberiacancode/reactuse";
+import { useClickOutside, useKeyPress } from "@siberiacancode/reactuse";
 import { useRouter } from "next/navigation";
 
 interface ArtistDrawerProps {
@@ -15,10 +15,13 @@ interface ArtistDrawerProps {
 const ArtistDrawer = ({ params }: ArtistDrawerProps) => {
   const router = useRouter();
   const pressedKeys = useKeyPress("Escape");
+  const ref = useClickOutside<HTMLDivElement>(() => {
+    router.back();
+  });
 
   return (
     <Drawer open={!pressedKeys} onClose={() => router.back()}>
-      <DrawerContent>{params.artistId}</DrawerContent>
+      <DrawerContent ref={ref}>{params.artistId}</DrawerContent>
     </Drawer>
   );
 };
