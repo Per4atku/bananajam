64ac6ea58e7b949dd1b0f1d87e4e2ca59c905f77
Per4atku <artcchi@gmail.com>
2024-07-17 00:39:50 +1000

    🍌 ArtistCard avatar and title

--- src/components/artist-card/ArtistCard.tsx
+++ src/components/artist-card/ArtistCard.tsx
@@ -9,12 +9,17 @@ const ArtistCard = async ({ artistId }: ArtistCardProps) => {
 
   if (artist)
     return (
-      <div className="w-full">
-        <div>
-          <Avatar>
-            <AvatarFallback>BJ</AvatarFallback>
-            <AvatarImage height={500} width={500} src={artist.images[0].url} />
+      <div className="w-full flex justify-center pt-12">
+        <div className="flex flex-col items-center gap-4 pb-4">
+          <Avatar className="w-40 h-40">
+            <AvatarFallback className="text-5xl">BJ</AvatarFallback>
+            <AvatarImage
+              width={160}
+              height={160}
+              src={artist.images.at(0)?.url}
+            />
           </Avatar>
+          <h1 className="text-center text-5xl font-bold">{artist.name}</h1>
         </div>
       </div>
     );
