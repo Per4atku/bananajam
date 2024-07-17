c22f867b127a36853a28b1f341774e577ef73600
Per4atku <artcchi@gmail.com>
2024-07-17 16:21:25 +1000

    🍌 Carousel migrate to swiper (card effect)

--- src/components/artist-card/AlbumCarousel.tsx
+++ src/components/artist-card/AlbumCarousel.tsx
@@ -12,48 +12,49 @@ import Image from "next/image";
 import { useQuery } from "@tanstack/react-query";
 import useCarousel from "@/hooks/useCarousel";
 import { useDidUpdate } from "@siberiacancode/reactuse";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards, EffectCube } from "swiper/modules";
+import { Card } from "../ui/card";
+
+import "swiper/css";
+import "swiper/css/effect-cards";
 
 interface AlbumCarouselProps {
   artistId: string;
 }
 
 export const AlbumCarousel = ({ artistId }: AlbumCarouselProps) => {
-  const { setApi, current, count } = useCarousel();
   const { data } = useQuery({
     queryKey: [artistId, "get-artists-albums"],
     queryFn: async () => (await musicDataAPI.getArtistsAlbums(artistId)).data,
   });
-  useDidUpdate(() => {
-    console.log(current), [current];
-  });
   if (data) {
     return (
-      <Carousel
-        setApi={setApi}
-        className="w-full flex items-center "
-        opts={{
-          align: "start",
-          loop: true,
-        }}
+      <Swiper
+        grabCursor
+        modules={[EffectCards]}
+        effect="cards"
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
       >
-        <CarouselContent>
-          {data.items.map((album, index) => (
-            <CarouselItem className="basis-1/3 " key={index}>
+        {data.items.map((album, index) => (
+          <SwiperSlide key={index}>
+            <Card className=" border-primary  w-80 h-80 flex items-center justify-center">
               <div className="flex flex-col items-center gap-4">
                 <Image
                   className="rounded-lg"
-                  width={120}
-                  height={120}
+                  width={200}
+                  height={200}
                   src={album.images.at(1)?.url || ""}
                   alt={`${album.name} Cover`}
                 />
-                <h3 className="text-center text-sm  font-bold">{album.name}</h3>
-                <p>{index === current && "Current"}</p>
+                <h3 className="text-center  font-bold">{album.name}</h3>
               </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     );
   }
 };
