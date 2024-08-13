4ee07b2eb6e6e10f2686de06bec1dd5c58a30b87
Per4atku <artcchi@gmail.com>
2024-08-13 14:51:00 +0700

    🍌 responsive layout for artistcard header

--- src/components/artist-card/ArtistCard.tsx
+++ src/components/artist-card/ArtistCard.tsx
@@ -5,6 +5,7 @@ import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { getInitials } from "@/lib/utils";
 import Artist from "../items/Artist";
 import { AlbumCarousel } from "./AlbumCarousel";
+import { useMediaQuery } from "@siberiacancode/reactuse";
 
 interface ArtistCardProps {
   artistId: string;
@@ -15,18 +16,16 @@ const ArtistCard = async ({ artistId }: ArtistCardProps) => {
   if (artist)
     return (
       <div className="w-full flex justify-center pt-12 flex-col gap-4">
-        <div className="flex flex-col items-center gap-4 pb-4">
-          <Avatar className="w-40 h-40">
+        <div className="flex flex-col items-center gap-2 pb-4 sm:gap-4">
+          <Avatar className="w-32 h-32 sm:w-40 sm:h-40">
             <AvatarFallback className="text-5xl">
               {getInitials(artist.name)}
             </AvatarFallback>
-            <AvatarImage
-              width={160}
-              height={160}
-              src={artist.images.at(1)?.url}
-            />
+            <AvatarImage src={artist.images.at(1)?.url} />
           </Avatar>
-          <h1 className="text-center text-5xl font-bold">{artist.name}</h1>
+          <h1 className="text-center text-3xl font-bold sm:text-5xl">
+            {artist.name}
+          </h1>
           <p className=" text-center text-muted-foreground italic max-w-sm">
             {artist.genres.toString()}
           </p>
