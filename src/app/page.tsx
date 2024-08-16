a70fbcf48d10d9b3b7282b4de90e6f1ac53ca7c5
Per4atku <artcchi@gmail.com>
2024-08-16 14:52:17 +0700

    🍌 link to /search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function Home() {
   return (
     <div>
       <p>Home Page!</p>
+      <Link href={"/search"} className={buttonVariants()}>
+        Go to search page
+      </Link>
     </div>
   );
 }
