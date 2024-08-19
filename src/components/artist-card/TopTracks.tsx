fb16dc756127936d2d33c47a1e17d57ae946c3fe
Per4atku <artcchi@gmail.com>
2024-08-19 12:42:42 +0700

    🍌 top tracks layout fixes

--- src/components/artist-card/TopTracks.tsx
+++ src/components/artist-card/TopTracks.tsx
@@ -11,24 +11,30 @@ const TopTracks = async ({ artistId }: TopTracksProps) => {
   const topTracks = (await musicDataAPI.getArtistTopTracks(artistId)).data;
 
   return (
-    <div>
-      {topTracks.tracks.slice(0, 5).map((track, index) => (
-        <React.Fragment key={index}>
-          <div className="flex items-center" key={index}>
-            <p className="">{index + 1}</p>
-            <Track
-              id={track.id}
-              imageURL={track.album.images.at(2)?.url || ""}
-              name={track.name}
-              isAdded={false}
-              artistName={track.artists.map((artist) => artist.name).toString()}
-              albumName={track.album.name}
-              className="px-2 my-1.5"
-            />
-          </div>
-          {index + 1 !== 5 && <Separator className=" bg-primary opacity-30" />}
-        </React.Fragment>
-      ))}
+    <div className="w-full flex justify-center">
+      <div className="w-full max-w-[600px] ">
+        {topTracks.tracks.slice(0, 5).map((track, index) => (
+          <React.Fragment key={index}>
+            <div className="flex items-center" key={index}>
+              <p className=" font-bold text-lg mr-2">{index + 1}</p>
+              <Track
+                id={track.id}
+                imageURL={track.album.images.at(2)?.url || ""}
+                name={track.name}
+                isAdded={false}
+                artistName={track.artists
+                  .map((artist) => artist.name)
+                  .toString()}
+                albumName={track.album.name}
+                className="px-2 my-1.5"
+              />
+            </div>
+            {index + 1 !== 5 && (
+              <Separator className=" bg-primary opacity-30" />
+            )}
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 };
