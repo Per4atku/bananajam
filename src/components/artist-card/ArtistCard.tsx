1144221f4e699f9b410aef84d545125dc8fc2eaf
Per4atku <artcchi@gmail.com>
2024-08-31 01:23:34 +1000

    🍌 finally get rid of axios (move to http-client

--- src/components/artist-card/ArtistCard.tsx
+++ src/components/artist-card/ArtistCard.tsx
@@ -1,15 +1,27 @@
-import { musicDataAPI } from "@/apis/musicDataAPI";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { getInitials } from "@/lib/utils";
-import { AlbumCarousel } from "./AlbumCarousel";
-import TopTracks from "./TopTracks";
-import Loader from "../Loader";
+import { OneArtistResponse } from "@/generated/api"
+import { getInitials } from "@/lib/utils"
+import { spotifyApi } from "@/utils/api/instance"
+import { cookies } from "next/headers"
+
+import Loader from "../Loader"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+import { AlbumCarousel } from "./AlbumCarousel"
+import TopTracks from "./TopTracks"
 
 interface ArtistCardProps {
-  artistId: string;
+  artistId: string
 }
 const ArtistCard = async ({ artistId }: ArtistCardProps) => {
-  const artist = (await musicDataAPI.getArtistById(artistId)).data;
+  const cookieStore = cookies()
+  const artist: OneArtistResponse = await spotifyApi.get(
+    `artists/${artistId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${cookieStore.get("spotify_access_token")?.value}`,
+      },
+      cache: "no-cache",
+    },
+  )
 
   if (artist)
     return (
@@ -17,24 +29,24 @@ const ArtistCard = async ({ artistId }: ArtistCardProps) => {
         <div className="flex flex-col items-center gap-2 pb-4 sm:gap-4">
           <Avatar className="w-32 h-32 sm:w-40 sm:h-40">
             <AvatarFallback className="text-5xl">
-              {getInitials(artist.name)}
+              {getInitials(artist.name!)}
             </AvatarFallback>
-            <AvatarImage src={artist.images.at(1)?.url} />
+            <AvatarImage src={artist.images!.at(1)?.url} />
           </Avatar>
           <h1 className="text-center text-3xl font-bold sm:text-5xl">
             {artist.name}
           </h1>
           <p className=" text-center text-muted-foreground italic max-w-sm">
-            {artist.genres.toString()}
+            {artist.genres!.toString()}
           </p>
         </div>
 
         <div className=" flex flex-col gap-4  sm:grid lg:grid-cols-2 lg:grid-rows-1">
-          <AlbumCarousel fallback={<Loader />} artistId={artist.id} />
+          <AlbumCarousel fallback={<Loader />} artistId={artist.id!} />
           <TopTracks artistId={artistId} />
         </div>
       </div>
-    );
-};
+    )
+}
 
-export default ArtistCard;
+export default ArtistCard
