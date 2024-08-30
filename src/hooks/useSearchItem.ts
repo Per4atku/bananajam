2b69fccc776424cedc85e32e24067ea7ba83adc5
Per4atku <artcchi@gmail.com>
2024-08-30 18:20:01 +1000

    🍌 orval - generated spotify api interfaces

--- src/hooks/useSearchItem.ts
+++ src/hooks/useSearchItem.ts
@@ -1,19 +1,19 @@
-import { Item, musicDataAPI } from "@/apis/musicDataAPI";
-import { useDidUpdate } from "@siberiacancode/reactuse";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { Item, musicDataAPI } from "@/apis/musicDataAPI"
+import { useDidUpdate } from "@siberiacancode/reactuse"
+import { useInfiniteQuery } from "@tanstack/react-query"
+import { useState } from "react"
 
 interface useSearchItemProps {
-  query: string;
-  sort_by: string;
-  itemsPerSroll: number;
+  query: string
+  sort_by: string
+  itemsPerSroll: number
 }
 
 interface useSearchItemReturn {
-  items: Item[];
-  isError: boolean;
-  error: Error | null;
-  fetchMore: () => void;
+  items: Item[]
+  isError: boolean
+  error: Error | null
+  fetchMore: () => void
 }
 
 export const useSearchItem = ({
@@ -21,7 +21,7 @@ export const useSearchItem = ({
   sort_by,
   itemsPerSroll,
 }: useSearchItemProps): useSearchItemReturn => {
-  const [items, setItems] = useState<Item[]>([]);
+  const [items, setItems] = useState<Item[]>([])
 
   const { isError, error, fetchNextPage } = useInfiniteQuery({
     queryKey: ["search-items", sort_by, query],
@@ -31,53 +31,53 @@ export const useSearchItem = ({
         limit: itemsPerSroll,
         query: query,
         offset: pageParam,
-      };
+      }
       switch (sort_by) {
         case "artist":
           const fetchedArtists = await musicDataAPI.getArtists({
             ...queryProps,
-          });
+          })
           setItems((prev) => {
-            if (prev) return [...prev, ...fetchedArtists];
-            else return [...fetchedArtists];
-          });
+            if (prev) return [...prev, ...fetchedArtists]
+            else return [...fetchedArtists]
+          })
 
-          return fetchedArtists;
+          return fetchedArtists
 
         case "album":
-          const fetchedAlbums = await musicDataAPI.getAlbums({ ...queryProps });
+          const fetchedAlbums = await musicDataAPI.getAlbums({ ...queryProps })
           setItems((prev) => {
-            if (prev) return [...prev, ...fetchedAlbums];
-            else return [...fetchedAlbums];
-          });
+            if (prev) return [...prev, ...fetchedAlbums]
+            else return [...fetchedAlbums]
+          })
 
-          return fetchedAlbums;
+          return fetchedAlbums
 
         case "track":
-          const fetchedTracks = await musicDataAPI.getTracks({ ...queryProps });
+          const fetchedTracks = await musicDataAPI.getTracks({ ...queryProps })
           setItems((prev) => {
-            if (prev) return [...prev, ...fetchedTracks];
-            else return [...fetchedTracks];
-          });
-          return fetchedTracks;
+            if (prev) return [...prev, ...fetchedTracks]
+            else return [...fetchedTracks]
+          })
+          return fetchedTracks
       }
     },
     getNextPageParam: (lastPage, allPages, lastPageParam) => {
       if (lastPage?.length === 0) {
-        return undefined;
+        return undefined
       }
-      return lastPageParam + itemsPerSroll;
+      return lastPageParam + itemsPerSroll
     },
-  });
+  })
 
   useDidUpdate(() => {
-    setItems([]);
-  }, [query, sort_by]);
+    setItems([])
+  }, [query, sort_by])
 
   return {
     items,
     isError,
     error,
     fetchMore: fetchNextPage,
-  };
-};
+  }
+}
