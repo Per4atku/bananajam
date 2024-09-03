95ba58993f1a120ad9903413b164a8489a07c64c
Per4atku <artcchi@gmail.com>
2024-09-03 20:45:38 +1000

    🍌 Minor middleware change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,24 @@
 import { auth } from "@/auth"
 
-import { PUBLIC_ROUTES, REDIRECT_TO_LOGIN } from "./lib/routes"
+import {
+  PUBLIC_ROUTES,
+  REDIRECT_AFTER_LOGIN,
+  REDIRECT_TO_LOGIN,
+} from "./lib/routes"
 
 export default auth((req) => {
   const { nextUrl } = req
   const isAuthenticated = !!req.auth
   const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname)
+  const isLoginRoute = nextUrl.pathname === "/login"
 
   console.log(isAuthenticated, isPublicRoute)
   console.log(nextUrl.pathname)
 
   if (!isAuthenticated && !isPublicRoute)
     return Response.redirect(new URL(REDIRECT_TO_LOGIN, nextUrl))
+  else if (isAuthenticated && isLoginRoute)
+    return Response.redirect(new URL(REDIRECT_AFTER_LOGIN, nextUrl))
 })
 
 // Optionally, don't invoke Middleware on some paths
